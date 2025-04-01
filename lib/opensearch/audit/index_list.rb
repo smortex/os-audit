ff12159fe989b1f3294a003e5f285c3542e37f1a
Romain Tartière <romain@blogreen.org>
2025-04-01 09:02:45 -1000

    Vertically align indices summary
    Adjust the width of columns for indices names it the short summary so
    that all data is properly aligned regrarless of the length of the
    indices names.

--- lib/opensearch/audit/index_list.rb
+++ lib/opensearch/audit/index_list.rb
@@ -1,12 +1,16 @@
 module OpenSearch
   module Audit
     class IndexList
+      attr_reader :longest_index_name
+
       def initialize
         @hash = Hash.new { |hash, key| hash[key] = IndexGroup.new }
+        @longest_index_name = 0
       end
 
       def add(index)
         @hash[index.group_name].add(index)
+        @longest_index_name = [@longest_index_name, index.name.length].max
       end
 
       def each(&block)
