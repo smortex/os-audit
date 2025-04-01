f95eb4fc59edca30c0835f7f8a11b9417678adb7
Romain Tartière <romain@blogreen.org>
2025-03-31 17:05:37 -1000

    Add a check for unexpected data types
    A field named "ip" should have a type to store IP addresses.  Likewise,
    a field name port should probably not be of type wildcard or text.
    
    Identify a bunch of common field names and the corresponding types to
    check for errors.

---
+++ lib/opensearch/audit/checks/unexpected_type.rb
@@ -0,0 +1,68 @@
+module OpenSearch
+  module Audit
+    module Checks
+      class UnexpectedType < Base
+        class Offense
+          attr_reader :field, :actual
+
+          def initialize(field, actual, expected)
+            @field = field
+            @actual = actual
+            @expected = expected
+          end
+
+          def expected
+            if @expected.count > 1
+              "one of #{@expected.join(", ")}"
+            else
+              @expected.first
+            end
+          end
+
+          def to_s
+            "Field #{field} is of type #{actual} but should be #{expected}"
+          end
+        end
+
+        def check
+          @index_list.each do |_group_name, indices|
+            indices.each do |index|
+              offenses = unexpected_types(index.mapping)
+              if offenses.any?
+                logger.warn "#{offenses.count} fields with unexpected types in #{index.name}"
+                offenses.each do |offense|
+                  logger.info "\t#{offense}"
+                end
+              end
+            end
+          end
+        end
+
+        def unexpected_types(mapping, key: [])
+          return [] unless mapping.is_a?(Hash)
+
+          result = []
+
+          if mapping.has_key?("type")
+            expected_types = case key.last
+            when "address" then ["ip", "keyword", "wildcard"]
+            when "ip", /_ip$/ then ["ip"]
+            when "port", /_port$/ then ["integer"]
+            when "uid", "gid" then ["integer"]
+            end
+
+            if expected_types && !expected_types.include?(mapping["type"])
+              result << Offense.new(key.join("."), mapping["type"], expected_types)
+            end
+          else
+            mapping.each do |k, v|
+              result += unexpected_types(v, key: key + [k])
+            end
+          end
+
+          result
+        end
+      end
+    end
+  end
+end
